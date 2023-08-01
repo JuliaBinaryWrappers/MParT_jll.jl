51f3e813b084745d074a12f540ff573dd76c6fef
jlbuild <juliabuildbot@gmail.com>
2023-08-01 04:02:34 +0000

    MParT_jll build 2.1.0+0

--- src/wrappers/x86_64-apple-darwin-julia_version+1.9.0.jl
+++ src/wrappers/x86_64-apple-darwin-julia_version+1.9.0.jl
@@ -4,12 +4,11 @@ export libmpart, libmpartjl
 using libcxxwrap_julia_jll
 using CompilerSupportLibraries_jll
 using Kokkos_jll
-using Eigen_jll
 JLLWrappers.@generate_wrapper_header("MParT")
 JLLWrappers.@declare_library_product(libmpart, "@rpath/libmpart.dylib")
 JLLWrappers.@declare_library_product(libmpartjl, "@rpath/libmpartjl.dylib")
 function __init__()
-    JLLWrappers.@generate_init_header(libcxxwrap_julia_jll, CompilerSupportLibraries_jll, Kokkos_jll, Eigen_jll)
+    JLLWrappers.@generate_init_header(libcxxwrap_julia_jll, CompilerSupportLibraries_jll, Kokkos_jll)
     JLLWrappers.@init_library_product(
         libmpart,
         "lib/libmpart.dylib",
